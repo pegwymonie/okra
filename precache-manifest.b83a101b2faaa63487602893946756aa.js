e5c20192ac635227df97bf9a79beb490e85a4699
pegwymonie <szurborg@gmail.com>
2019-05-14 23:04:53 -0400

    Updates

---
+++ precache-manifest.b83a101b2faaa63487602893946756aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cef8c97749b727d7297ed11d446e3b60",
+    "url": "/okra/index.html"
+  },
+  {
+    "revision": "421cfbcab41ac29305bf",
+    "url": "/okra/static/css/main.96c33caf.chunk.css"
+  },
+  {
+    "revision": "b086cfb872542f7cca55",
+    "url": "/okra/static/js/2.45f9182d.chunk.js"
+  },
+  {
+    "revision": "421cfbcab41ac29305bf",
+    "url": "/okra/static/js/main.f188b143.chunk.js"
+  },
+  {
+    "revision": "30fd5382d4677ad9074f",
+    "url": "/okra/static/js/runtime~main.0d98b766.js"
+  },
+  {
+    "revision": "d542261d05dd6568f5bc9b47ac1d8450",
+    "url": "/okra/static/media/GT.d542261d.png"
+  },
+  {
+    "revision": "db44b17aafc15401278c33fe9f88e59b",
+    "url": "/okra/static/media/rpnd.db44b17a.png"
+  }
+]);
