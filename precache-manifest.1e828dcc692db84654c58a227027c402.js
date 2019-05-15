27cd24c9ada2b92cc9f334de83c0d1a425e010a4
pegwymonie <szurborg@gmail.com>
2019-05-15 19:09:05 -0400

    Updates

--- precache-manifest.1e828dcc692db84654c58a227027c402.js
+++ precache-manifest.1e828dcc692db84654c58a227027c402.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "939e169e7e40a7c1fc93d158c274812d",
+    "revision": "c86288d459f0858a5bef8d0533872c29",
     "url": "/okra/index.html"
   },
   {
-    "revision": "4d28fe9dd58680464728",
+    "revision": "51186668814f1a3cd33f",
     "url": "/okra/static/css/main.96c33caf.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/okra/static/js/2.45f9182d.chunk.js"
   },
   {
-    "revision": "4d28fe9dd58680464728",
-    "url": "/okra/static/js/main.ef3e2d7a.chunk.js"
+    "revision": "51186668814f1a3cd33f",
+    "url": "/okra/static/js/main.3ce293cb.chunk.js"
   },
   {
     "revision": "30fd5382d4677ad9074f",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/okra/precache-manifest.60c6d23159dfd68cd283fe3c382f6739.js"
+  "/okra/precache-manifest.1e828dcc692db84654c58a227027c402.js"
 );
 
 self.addEventListener('message', (event) => {
